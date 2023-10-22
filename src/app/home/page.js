90c6a91f0bc36c0e2c81546c31a83be84d83f165
OCEAN <ocean@kudounoMacBook-puro.local>
2023-10-22 22:36:53 +0900

    1022

---
+++ src/app/home/page.js
@@ -0,0 +1,18 @@
+// app/home/page.js
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to HelpLife</h1>
+        <p>Your journey to explore side hustles begins here.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+// components/Footer.js
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 HelpLife</p>
+    </footer>
+  );
+}
+
+export default Footer;
